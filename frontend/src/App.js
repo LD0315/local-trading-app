dee2e70ac883d114d1b400e99c878c3e8282c70f
Linlin <linlin@d-i89-174-113.student.eduroam.uq.edu.au>
2020-12-21 15:49:05 +1000

    frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,202 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+    <header className="row">
+        <div>
+            <a className="brand" href="index.html">Local Trading</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+    <main>
+       <div className="row center">
+           <div className="card">
+               <a href="product.html">
+                
+                 <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />  
+               </a>
+               <div className="card-body">
+                   <a href="sweetcorn.html">
+                       <h2>Sweetcorn</h2>
+                   </a>
+                   <div className="rating">
+                       <span> <i className="fa fa-star"></i> </span>
+                       <span> <i className="fa fa-star"></i> </span>
+                       <span> <i className="fa fa-star"></i> </span>
+                       <span> <i className="fa fa-star"></i>  </span>
+                       <span> <i className="fa fa-star"></i> </span>   
+                   </div>
+                   <div className="price">
+                       $ 3
+                   </div>
+               </div>
+           </div>
+
+           <div className="card">
+            <a href="product.html">
+           
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="fa fa-star"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+
+        <div className="card">
+            <a href="product.html">
+            
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="fa fa-star"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+
+        <div className="card">
+            <a href="product.html">
+            
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="fa fa-star"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+
+        <div className="card">
+            <a href="product.html">
+            
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="fa fa-star"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+
+        <div className="card">
+            <a href="product.html">
+           
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="fa fa-star"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+
+        <div className="card">
+            <a href="product.html">
+             
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="fas fa-star-half-alt"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+
+        <div className="card">
+            <a href="product.html">
+            
+              <img className="medium" src="./images/sweetcorn.jpg" alt="sweetcorn" />
+            </a>
+            <div className="card-body">
+                <a href="sweetcorn.html">
+                    <h2>Sweetcorn</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i>  </span>
+                    <span> <i className="far fa-star"></i> </span>   
+                </div>
+                <div className="price">
+                    $ 3
+                </div>
+            </div>
+        </div>
+       </div>
+    </main>
+    <footer className="row center">
+        All right reserved
+    </footer>
+</div>
+   
+  );
+}
+
+export default App;
